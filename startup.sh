7a66e22154cb5af2eebe2928e2e8e22505e07a05
ZhangLei <RyanCheung98@163.com>
2021-11-27 19:33:46 +0800

    :tada: initial project

--- startup.sh
+++ startup.sh
@@ -4,10 +4,10 @@ export CUDA_VISIBLE_DEVICES=0
 
 nohup python3 main.py --experiments ./configs/experiment_sm.json \
                                     ./configs/experiment_mm.json \
-                                    ./configs/experiment_fedavg.json\
-                                    ./configs/experiment_fedprox.json \
                                     ./configs/experiment_ewc.json \
                                     ./configs/experiment_mas.json \
+                                    ./configs/experiment_fedavg.json\
+                                    ./configs/experiment_fedprox.json \
                                     > task-1.log 2>&1 &
 
 nohup python3 main.py --experiments ./configs/experiment_fedweit.json \
