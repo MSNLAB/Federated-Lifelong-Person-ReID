232c977b6b5943ac8640e96fa96a45c17e63508d
ZhangLei <RyanCheung98@163.com>
2022-02-19 22:31:33 +0800

    :alembic: improve the experiments

--- startup.sh
+++ startup.sh
@@ -7,4 +7,5 @@ nohup python3 main.py --experiments ./configs/basis_exp/experiment_sm.yaml \
                                     ./configs/basis_exp/experiment_fedprox.yaml \
                                     ./configs/basis_exp/experiment_fedcurv.yaml \
                                     ./configs/basis_exp/experiment_fedweit.yaml \
+                                    ./configs/basis_exp/experiment_fedstil.yaml \
                                     > task.log 2>&1 &
